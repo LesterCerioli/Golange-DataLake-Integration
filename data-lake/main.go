7cced1344c15714ce76956e33665c4514659db6c
Lester Cerioli <lesterlucasit@hotmail.com>
2024-09-23 00:53:54 -0700

    Building Sql script and authentication service

--- data-lake/main.go
+++ data-lake/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"data-lake/services"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Post("/loign", services.Login)
+	app.Get("/secure-data", services.SecureData)
+
+	log.Fatal(app.Listen(":3000"))
+}
